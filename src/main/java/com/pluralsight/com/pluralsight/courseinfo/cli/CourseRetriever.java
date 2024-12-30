d59ff858ccd1e210892696322db410e818ffa7ac
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-12-30 16:37:32 +0100

    This is my first commit.

---
+++ src/main/java/com/pluralsight/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,7 @@
+package com.pluralsight.com.pluralsight.courseinfo.cli;
+
+public class CourseRetriever {
+    public static void main(String[] args) {
+        System.out.println("  CourseRetriever started ...  ");
+    }
+}
